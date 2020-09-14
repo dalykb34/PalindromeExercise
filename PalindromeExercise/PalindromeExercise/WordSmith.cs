a0a6f268c5817f3de4cb37381258388a202d0a3d
Kevin Daly <dalykb34@gmail.com>
2020-09-14 15:20:43 -0500

    added exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            ////reverse the word
+            //var reversed = "";
+            //for(int i = word.Length - 1; i >= 0; i--)
+            //{
+            //    reversed += word[i];
+            //}
+            ////compare
+            //if (reversed.ToLower() == word.ToLower())
+            //{
+            //    return true;
+            //}
+            //return false;
+            return word.ToLower().SequenceEqual(word.ToLower().Reverse());
+        }
+    }
+}
